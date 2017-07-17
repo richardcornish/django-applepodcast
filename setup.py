e14082e4ec06c23f05789cc3e5bcda722cde4731
Richard Cornish <richardcornish@gmail.com>
2017-07-17 14:21:17 -0500

    Updated classifiers

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities'
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
