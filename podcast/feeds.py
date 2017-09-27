0941addbe21f5f5ea676ca89abe47180c7631492
Richard Cornish <richardcornish@gmail.com>
2017-09-27 17:08:06 -0500

    Added image tag to channel, fixes #20

--- podcast/feeds.py
+++ podcast/feeds.py
@@ -37,6 +37,11 @@ class ItunesFeed(Rss201rev2Feed):
     def add_root_elements(self, handler):
         super(ItunesFeed, self).add_root_elements(handler)
         handler.addQuickElement('copyright', self.feed['copyright'], escape=False, cdata=False)
+        handler.startElement('image', {})
+        handler.addQuickElement('url', self.feed['image'])
+        handler.addQuickElement('title', self.feed['title'])
+        handler.addQuickElement('link', self.feed['link'])
+        handler.endElement('image')
         handler.addQuickElement('itunes:type', self.feed['itunes']['type'])
         handler.addQuickElement('itunes:subtitle', self.feed['itunes']['subtitle'])
         handler.addQuickElement('itunes:summary', self.feed['itunes']['summary'], escape=False, cdata=True)
@@ -45,7 +50,7 @@ class ItunesFeed(Rss201rev2Feed):
         handler.addQuickElement('itunes:name', self.feed['itunes']['owner']['name'])
         handler.addQuickElement('itunes:email', self.feed['itunes']['owner']['email'])
         handler.endElement('itunes:owner')
-        handler.addQuickElement('itunes:image', '', {'href': self.feed['itunes']['image']})
+        handler.addQuickElement('itunes:image', '', {'href': self.feed['image']})
         for key, value in self.feed['itunes']['categories']:
             handler.startElement('itunes:category', {'text': key})
             for sub in value:
@@ -102,6 +107,7 @@ class ShowFeed(Feed):
     def feed_extra_kwargs(self, obj):
         return {
             'copyright': obj.get_copyright(),
+            'image': self.image(obj),
             'itunes': {
                 'type': obj.type,
                 'subtitle': obj.get_subtitle(),
@@ -114,7 +120,6 @@ class ShowFeed(Feed):
                     'name': obj.get_owner_name(),
                     'email': obj.get_owner_email(),
                 },
-                'image': self.image(obj),
                 'categories': obj.get_categories_dict(),
                 'explicit': obj.get_explicit(),
                 'block': obj.get_block(),
