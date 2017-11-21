bc826ead4e255c3428b79bb3bdc6fad0d0539084
Richard Cornish <richardcornish@gmail.com>
2017-11-21 16:08:28 -0600

    Fixed legacy itunespodcast references

--- docs/make.bat
+++ docs/make.bat
@@ -129,9 +129,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\django-itunespodcast.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\django-applepodcast.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\django-itunespodcast.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\django-applepodcast.ghc
 	goto end
 )
 
