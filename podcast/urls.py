70224f5e0f7ea8f7423de3efa777453410d41d5c
Richard Cornish <richardcornish@gmail.com>
2017-12-11 00:27:54 -0600

    Changed url to re_path for forward compatibility

--- podcast/urls.py
+++ podcast/urls.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 
 try:
-    from django.urls import include, path, re_path as url
+    from django.urls import include, re_path
 except ImportError:
-    from django.conf.urls import include, url
+    from django.conf.urls import include, url as re_path
 
 from .views import EpisodeDetailView, EpisodeDownloadView, ShowDetailView, ShowFeedView, ShowListView
 from . import settings
@@ -12,16 +12,16 @@ from . import settings
 app_name = 'podcast'
 
 singular = [
-    url(r'^feed/$', ShowFeedView.as_view(), name='show_feed'),
-    url(r'^(?P<slug>[\w-]+)/download/$', EpisodeDownloadView.as_view(), name='episode_download'),
-    url(r'^(?P<slug>[\w-]+)/$', EpisodeDetailView.as_view(), name='episode_detail'),
-    url(r'^$', ShowDetailView.as_view(), name='show_detail'),
+    re_path(r'^feed/$', ShowFeedView.as_view(), name='show_feed'),
+    re_path(r'^(?P<slug>[\w-]+)/download/$', EpisodeDownloadView.as_view(), name='episode_download'),
+    re_path(r'^(?P<slug>[\w-]+)/$', EpisodeDetailView.as_view(), name='episode_detail'),
+    re_path(r'^$', ShowDetailView.as_view(), name='show_detail'),
 ]
 
 if settings.PODCAST_SINGULAR:
     urlpatterns = singular
 else:
     urlpatterns = [
-        url(r'^(?P<show_slug>[\w-]+)/', include(singular)),
-        url(r'^$', ShowListView.as_view(), name='show_list'),
+        re_path(r'^(?P<show_slug>[\w-]+)/', include(singular)),
+        re_path(r'^$', ShowListView.as_view(), name='show_list'),
     ]
